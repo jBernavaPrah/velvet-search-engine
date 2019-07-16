18e99e1f591772d92df90143bcc054929cdc27aa
Jure Bernava Prah <webjure@gmail.com>
2019-07-16 14:16:57 +0200

    Fix App name typos

--- config/app.php
+++ config/app.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'name' => env('APP_NAME', 'Valvet - SearchEngine'),
+    'name' => env('APP_NAME', 'Velvet - SearchEngine'),
 
     /*
     |--------------------------------------------------------------------------
